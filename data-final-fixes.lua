981880107d2dde2932078a8cce7cd57364c75825
ldinc <drogunov.igor@gmail.com>
2020-05-18 01:45:17 +0300

    fix insert

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,2 +1,2 @@
 -- change recipes
-data.raw["recipe"]["apm_breeder_thorium_seperation_process_b"].normal.results.iserts({type="item", name="fusion-catalyst", amount=1})
+table.insert(data.raw["recipe"]["apm_breeder_thorium_seperation_process_b"].normal.results, {type="item", name="fusion-catalyst", amount=1})
